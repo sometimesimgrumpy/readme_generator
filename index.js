7a010cc90821579595de73401e4e02e18c697759
Brianna <briannaodell1437@gmail.com>
2022-07-31 16:54:53 -0700

    add email validator

--- index.js
+++ index.js
@@ -1,45 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
-
-const generateMarkdown = (questions) =>
-  `#${questions.projTitle}
-
-    ## Description
-
-    ##Table of Contents
-     - [Installation Instructions](#installation)
-     - [Usage](#usage)
-     - [Contributing](#contributing)
-     - [Tests](#tests)
-     - [License](#license)
-     - [Questions](#questions)
-
-    ## Installation
-    My project can be installed using the following terminal prompt:
-    
-    ${questions.installation}
-
-    ## Usage
-    
-    ${questions.projUse}
-
-    ## Contributing
-    
-    ${questions.contributing}
-
-    ## Tests
-    
-    ${questions.tests}
-
-    ## License
-    
-    This project is licensed with ${questions.license}.
-    ${getLicenseBadge(questions.license)}
-
-    ## Questions
-    If there are any additional question please contact: ${questions.email} 
-    Or visit the github repository at: https://github.com/${questions.gitUser}`;
+const validate = require("./utils/validator");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -97,31 +59,12 @@ const questions = [
     type: "input",
     message: "What is your contact email?",
     name: "email",
+    validate: validate(email),
   },
 ];
 
 // function to get license badges
 //https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
-
-function getLicenseBadge(license) {
-  if (license === "Apache License 2.0") {
-    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-  } else if (license === "Creative Commons") {
-    return `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`;
-  } else if (license === "GNU GPL v3") {
-    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
-  } else if (license === "ISC License") {
-    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
-  } else if (license === "MIT License") {
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-  } else if (license === "Mozilla Public License 2.0") {
-    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
-  } else if (license === "WTFPL License") {
-    return `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`;
-  } else if (license === "No License") {
-    return;
-  }
-}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, questions) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,96 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+//https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+
+function renderLicenseBadge(license) {
+  if (license === "Apache License 2.0") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "Creative Commons") {
+    return `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`;
+  } else if (license === "GNU GPL v3") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license === "ISC License") {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+  } else if (license === "MIT License") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license === "Mozilla Public License 2.0") {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (license === "WTFPL License") {
+    return `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`;
+  } else if (license === "No License") {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "Apache License 2.0") {
+    return `[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "Creative Commons") {
+    return `[Creative Commons License](http://creativecommons.org/publicdomain/zero/1.0/)`;
+  } else if (license === "GNU GPL v3") {
+    return `[GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license === "ISC License") {
+    return `[ISC License]https://opensource.org/licenses/ISC)`;
+  } else if (license === "MIT License") {
+    return `[MIT License](https://opensource.org/licenses/MIT)`;
+  } else if (license === "Mozilla Public License 2.0") {
+    return `[Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)`;
+  } else if (license === "WTFPL License") {
+    return `[License: WTFPL License](http://www.wtfpl.net/about/)`;
+  } else if (license === "No License") {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `
+  This project is licensed with ${renderLicenseLink(license)}.
+  ${renderLicenseBadge(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(questions) {
+  return `#${questions.projTitle}
 
-`;
+  ## Description
+
+  ##Table of Contents
+    - [Installation Instructions](#installation)
+    - [Usage](#usage)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [License](#license)
+    - [Questions](#questions)
+
+  ## Installation
+  My project can be installed using the following terminal prompt:
+  
+  ${questions.installation}
+
+  ## Usage
+  
+  ${questions.projUse}
+
+  ## Contributing
+  
+  ${questions.contributing}
+
+  ## Tests
+  
+  ${questions.tests}
+
+  ## License
+  
+  ${renderLicenseSection(questions.license)}
+
+  ## Questions
+  If there are any additional question please contact: ${questions.email} 
+  Or visit the github repository at: https://github.com/${questions.gitUser}`;
 }
 
 module.exports = generateMarkdown;
